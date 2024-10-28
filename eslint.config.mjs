efc1c43a158696f558b313c1226538f2c0a6d410
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-10-28 17:01:39 -0600

    Implemented correct eslint file configuration for exercise 11.6

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import globals from "globals";
+import stylisticJs from "@stylistic/eslint-plugin-js";
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import jest from "eslint-plugin-jest";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      sourceType: "module", // Ensure ESLint understands ES module syntax
+      ecmaVersion: 2018,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      "@stylistic/js": stylisticJs,
+      react,
+      jest,
+    },
+    rules: {
+      "@stylistic/js/indent": ["error", 2],
+      "@stylistic/js/linebreak-style": ["error", "unix"],
+      "@stylistic/js/quotes": ["error", "single"],
+      "@stylistic/js/semi": ["error", "never"],
+      eqeqeq: "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+      "arrow-spacing": ["error", { before: true, after: true }],
+      "no-console": "off",
+    },
+  },
+  {
+    ignores: ["dist/**", "build/**", "eslint.config.mjs"], // Ignore your config file
+  },
+];
